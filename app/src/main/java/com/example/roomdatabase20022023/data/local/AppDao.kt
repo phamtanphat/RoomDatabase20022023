e4c5c62a97456b0a4821dd0ae71facb1ce83e30e
Phat <phatdroid94@gmail.com>
2023-05-17 21:39:00 +0700

    database

---
+++ app/src/main/java/com/example/roomdatabase20022023/data/local/AppDao.kt
@@ -0,0 +1,10 @@
+package com.example.roomdatabase20022023.data.local
+
+import androidx.room.Dao
+
+/**
+ * Created by pphat on 5/17/2023.
+ */
+
+@Dao
+interface AppDao {}
---
+++ app/src/main/java/com/example/roomdatabase20022023/data/local/AppDatabase.kt
@@ -0,0 +1,33 @@
+package com.example.roomdatabase20022023.data.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.roomdatabase20022023.data.local.entity.Priority
+import com.example.roomdatabase20022023.data.local.entity.WorkEntity
+
+/**
+ * Created by pphat on 5/17/2023.
+ */
+
+@Database(entities = [WorkEntity::class, Priority::class], version = 1)
+abstract class AppDatabase: RoomDatabase() {
+    abstract fun appDao(): AppDao
+
+    companion object {
+        private var instance: AppDatabase? = null
+
+        fun createDatabase(context: Context): AppDatabase {
+            if (instance == null) {
+                instance = Room.databaseBuilder(
+                    context,
+                    AppDatabase::class.java,
+                    "app_database"
+                ).build()
+            }
+            return instance as AppDatabase
+        }
+    }
+
+}
