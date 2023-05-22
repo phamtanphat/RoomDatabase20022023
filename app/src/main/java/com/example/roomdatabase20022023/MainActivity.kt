6b5d4b3f03e9e8cc517733957c42c462618e3f13
Phat <phatdroid94@gmail.com>
2023-05-22 20:07:19 +0700

    update

--- app/src/main/java/com/example/roomdatabase20022023/MainActivity.kt
+++ app/src/main/java/com/example/roomdatabase20022023/MainActivity.kt
@@ -11,16 +11,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        CoroutineScope(Dispatchers.IO).launch {
-            val appDao = AppDatabase.createDatabase(this@MainActivity)?.appDao()
-//            val listWorks = appDao?.getListWorks()
-//            Log.d("BBB", listWorks?.size.toString())
-//            appDao?.addWorks(WorkEntity(0, "Do something", System.currentTimeMillis(), Priority(0, PriorityEnum.LOW)))
-
-            val result = async { appDao?.getPriority() }
-            withContext(Dispatchers.Main) {
-               Toast.makeText(this@MainActivity,  result.await()?.size.toString(), Toast.LENGTH_SHORT).show()
-            }
-        }
     }
 }
